fa3ddc88098422e22e3de82057ac987ab2ddf932
jpabloallin <jpac.647@gmail.com>
2022-05-20 16:56:33 -0500

    Added behaviors and appended domain events for each aggregate.

--- src/main/java/com/sofkaU/medellinLabDDD/lab/Lab.java
+++ src/main/java/com/sofkaU/medellinLabDDD/lab/Lab.java
@@ -1,10 +1,10 @@
 package com.sofkaU.medellinLabDDD.lab;
 
 import co.com.sofka.domain.generic.AggregateEvent;
-import com.sofkaU.medellinLabDDD.lab.events.LabCreated;
-import com.sofkaU.medellinLabDDD.lab.values.LabId;
-import com.sofkaU.medellinLabDDD.lab.values.LabType;
+import com.sofkaU.medellinLabDDD.lab.events.*;
+import com.sofkaU.medellinLabDDD.lab.values.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class Lab extends AggregateEvent<LabId> {
@@ -16,5 +16,40 @@ public class Lab extends AggregateEvent<LabId> {
     public Lab(LabId entityId, LabType labType) {
         super(entityId);
         appendChange(new LabCreated(labType)).apply();
+    }
+    public void updateLabType(LabType labType) {
+        appendChange(new LabTypeUpdated(labType)).apply();
+    }
+    public void addDevice(DeviceId entityId, DeviceName deviceName, Model model, LifeSpan lifeSpan) {
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(deviceName);
+        Objects.requireNonNull(model);
+        Objects.requireNonNull(lifeSpan);
+        appendChange(new DeviceAdded(entityId, deviceName, model, lifeSpan)).apply();
+    }
+    public void updateDeviceName(DeviceId entityId, DeviceName deviceName) {
+        appendChange(new DeviceNameUpdated(entityId, deviceName)).apply();
+    }
+    public void updateDeviceModel(DeviceId entityId, Model model) {
+        appendChange(new DeviceModelUpdated(entityId, model)).apply();
+    }
+    public void updateDeviceLifeSpan(DeviceId entityId, LifeSpan lifeSpan) {
+        appendChange(new DeviceLifeSpanUpdated(entityId, lifeSpan)).apply();
+    }
+    public void addBacteriologist(BacteriologistId entityId, Name name) {
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+        appendChange(new DeviceAdded(entityId, name)).apply();
+    }
+    public void updateBacteriologistName(BacteriologistId entityId, Name name) {
+        appendChange(new BacteriologistNameUpdated(entityId, name)).apply();
+    }
+    public void addBioengineer(BioengineerId entityId, Name name) {
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+        appendChange(new DeviceAdded(entityId, name)).apply();
+    }
+    public void updateBioengineerName(BioengineerId entityId, Name name) {
+        appendChange(new BioengineerNameUpdated(entityId, name)).apply();
     }
 }
--- src/main/java/com/sofkaU/medellinLabDDD/staff/Staff.java
+++ src/main/java/com/sofkaU/medellinLabDDD/staff/Staff.java
@@ -1,10 +1,15 @@
 package com.sofkaU.medellinLabDDD.staff;
 
 import co.com.sofka.domain.generic.AggregateEvent;
-import com.sofkaU.medellinLabDDD.staff.events.StaffCreated;
-import com.sofkaU.medellinLabDDD.staff.values.StaffId;
-import com.sofkaU.medellinLabDDD.staff.values.WorkArea;
+import com.sofkaU.medellinLabDDD.lab.events.DeviceModelUpdated;
+import com.sofkaU.medellinLabDDD.lab.events.DeviceNameUpdated;
+import com.sofkaU.medellinLabDDD.lab.events.LabTypeUpdated;
+import com.sofkaU.medellinLabDDD.lab.values.*;
+import com.sofkaU.medellinLabDDD.lab.values.Name;
+import com.sofkaU.medellinLabDDD.staff.events.*;
+import com.sofkaU.medellinLabDDD.staff.values.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class Staff  extends AggregateEvent<StaffId> {
@@ -15,5 +20,45 @@ public class Staff  extends AggregateEvent<StaffId> {
     public Staff(StaffId entityId, WorkArea workArea) {
         super(entityId);
         appendChange(new StaffCreated(workArea)).apply();
+    }
+    public void updateWorkArea(WorkArea workArea) {
+        appendChange(new WorkAreaUpdated(workArea)).apply();
+    }
+    public void addLabCourier(LabCourierId entityId, Name name, YearsOfExperience yearsOfExperience) {
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(yearsOfExperience);
+        appendChange(new LabCourierAdded(entityId, name, yearsOfExperience)).apply();
+    }
+    public void updateLabCourierName(LabCourierId entityId, Name name) {
+        appendChange(new LabCourierNameUpdated(entityId, name)).apply();
+    }
+    public void updateLabCourierYearsOfExperience(LabCourierId entityId, YearsOfExperience yearsOfExperience) {
+        appendChange(new LabCourierYearsOfExperienceUpdated(entityId, yearsOfExperience)).apply();
+    }
+
+    public void addCleaner(CleanerId entityId, Name name, YearsOfExperience yearsOfExperience) {
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(yearsOfExperience);
+        appendChange(new CleanerAdded(entityId, name, yearsOfExperience)).apply();
+    }
+    public void updateCleanerName(CleanerId entityId, Name name) {
+        appendChange(new CleanerNameUpdated(entityId, name)).apply();
+    }
+    public void updateCleanerYearsOfExperience(CleanerId entityId, YearsOfExperience yearsOfExperience) {
+        appendChange(new CleanerYearsOfExperienceUpdated(entityId, yearsOfExperience)).apply();
+    }
+    public void addRecepcionist(RecepcionistId entityId, Name name, YearsOfExperience yearsOfExperience) {
+        Objects.requireNonNull(entityId);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(yearsOfExperience);
+        appendChange(new RecepcionistAdded(entityId, name, yearsOfExperience)).apply();
+    }
+    public void updateRecepcionistName(RecepcionistId entityId, Name name) {
+        appendChange(new RecepcionistNameUpdated(entityId, name)).apply();
+    }
+    public void updateRecepcionistYearsOfExperience(RecepcionistId entityId, YearsOfExperience yearsOfExperience) {
+        appendChange(new RecepcionistYearsOfExperienceUpdated(entityId, yearsOfExperience)).apply();
     }
 }
