0e1ce369d6a376794529675e59e34a6c2667d7c5
jpabloallin <jpac.647@gmail.com>
2022-05-19 22:16:43 -0500

    Initial commit

---
+++ src/main/java/com/sofkaU/medellinLabDDD/MedellinLabDddApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.medellinLabDDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedellinLabDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedellinLabDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/medellinLabDDD/MedellinLabDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.medellinLabDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedellinLabDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
