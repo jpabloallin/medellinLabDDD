6d2e6fbc34e193b6d6b9e6465a8176d2a0b897e2
jpabloallin <jpac.647@gmail.com>
2022-05-20 15:32:31 -0500

    Added behaviors of the entities on my Staff aggregate.

--- src/main/java/com/sofkaU/medellinLabDDD/staff/Cleaner.java
+++ src/main/java/com/sofkaU/medellinLabDDD/staff/Cleaner.java
@@ -5,20 +5,26 @@ import com.sofkaU.medellinLabDDD.lab.values.Name;
 import com.sofkaU.medellinLabDDD.staff.values.CleanerId;
 import com.sofkaU.medellinLabDDD.staff.values.YearsOfExperience;
 
+import java.util.Objects;
+
 public class Cleaner extends Entity<CleanerId> {
-    private final Name name;
-    private final YearsOfExperience yearsOfExperience;
+    private Name name;
+    private YearsOfExperience yearsOfExperience;
 
     public Cleaner(CleanerId entityId, Name name, YearsOfExperience yearsOfExperience) {
         super(entityId);
         this.name = name;
         this.yearsOfExperience = yearsOfExperience;
     }
-
+    public void updateName(Name name) {
+        this.name = Objects.requireNonNull(name);
+    }
+    public void updateYearsOfExperience(YearsOfExperience yearsOfExperience) {
+        this.yearsOfExperience = Objects.requireNonNull(yearsOfExperience);
+    }
     public Name name() {
         return name;
     }
-
     public YearsOfExperience yearsOfExperience() {
         return yearsOfExperience;
     }
--- src/main/java/com/sofkaU/medellinLabDDD/staff/LabCourier.java
+++ src/main/java/com/sofkaU/medellinLabDDD/staff/LabCourier.java
@@ -5,16 +5,23 @@ import com.sofkaU.medellinLabDDD.lab.values.Name;
 import com.sofkaU.medellinLabDDD.staff.values.LabCourierId;
 import com.sofkaU.medellinLabDDD.staff.values.YearsOfExperience;
 
+import java.util.Objects;
+
 public class LabCourier extends Entity<LabCourierId> {
-    private final Name name;
-    private final YearsOfExperience yearsOfExperience;
+    private Name name;
+    private YearsOfExperience yearsOfExperience;
 
     public LabCourier(LabCourierId entityId, Name name, YearsOfExperience yearsOfExperience) {
         super(entityId);
         this.name = name;
         this.yearsOfExperience = yearsOfExperience;
     }
-
+    public void updateName(Name name) {
+        this.name = Objects.requireNonNull(name);
+    }
+    public void updateYearsOfExperience(YearsOfExperience yearsOfExperience) {
+        this.yearsOfExperience = Objects.requireNonNull(yearsOfExperience);
+    }
     public Name name() {
         return name;
     }
